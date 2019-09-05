a3dfe7083673db4eca3efb390d6959b5cdba276c
tomorrowzjz <1047547064@qq.com>
2019-09-05 18:34:04 +0800

    'addpromiseA+'

--- myPromise/test.js
+++ myPromise/test.js
@@ -1,13 +1,31 @@
-let Promise = require('./promise')
+let Promise = require('./promiseA+')
 let p = new Promise((resolve,reject)=>{
   setTimeout(()=>{
-    reject('err')
+    resolve('err1')
   },1000)
   // throw new Error('test')
   // resolve('nihao');
 })
-p.then((data)=>{
-  console.log(data);
+p.then((res)=>{
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve(res)
+    },1000)
+  }).then((zjzj)=>{
+    console.log('1'+zjzj);
+  },err=>{
+    console.log(err);
+  })
+},(err)=>{
+  console.log(err);
+  return 1
+}).then((zjz)=>{
+  console.log(zjz);
+  return 2
+},(err)=>{
+  console.log(err);
+}).then(ss=>{
+  console.log(ss);
 },(err)=>{
   console.log(err);
 })
